845c86ad2ad15fdf280619d4d901064560b096ae
sweeneycai <juanxincai0526@gmail.com>
2023-12-27 08:19:15 +0800

    [add]Add promo live view.

---
+++ lib/pento_web/live/promo_live.ex
@@ -0,0 +1,54 @@
+defmodule PentoWeb.PromoLive do
+  use PentoWeb, :live_view
+
+  alias Pento.Promo
+  alias Pento.Promo.Recipient
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, socket |> assign_recipient() |> clear_form()}
+  end
+
+  def assign_recipient(socket) do
+    socket
+    |> assign(:recipient, %Recipient{})
+  end
+
+  def clear_form(socket) do
+    form =
+      socket.assigns.recipient
+      |> Promo.change_recipient()
+      |> to_form()
+
+    assign(socket, :form, form)
+  end
+
+  @doc """
+  Assign `:form` to form changeset.
+  """
+  def assign_form(socket, changeset) do
+    assign(socket, :form, to_form(changeset))
+  end
+
+  @impl true
+  def handle_event(
+        "validate",
+        %{"recipient" => recipient_params},
+        %{assigns: %{recipient: recipient}} = socket
+      ) do
+    changeset =
+      recipient
+      |> Promo.change_recipient(recipient_params)
+      |> Map.put(:action, :validate)
+
+    {:noreply, assign_form(socket, changeset)}
+  end
+
+  def handle_event(
+        "save",
+        %{"recipient" => recipient_params},
+        %{assigns: %{recipient: recipient}} = socket
+      ) do
+    :timer.sleep(2000)
+  end
+end
