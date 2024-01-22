8443143380df4d553b424a3588e2e8b6811b817d
sweeneycai <juanxincai0526@gmail.com>
2024-01-23 00:17:58 +0800

    [add][survey]add survey rating

--- lib/pento_web/live/survey_live.ex
+++ lib/pento_web/live/survey_live.ex
@@ -3,7 +3,8 @@ defmodule PentoWeb.SurveyLive do
 
   alias __MODULE__.Component
   alias PentoWeb.DemographicLive
-  alias PentoWeb.DemographicLive.Show
+  alias PentoWeb.RatingLive
+  alias Pento.Catalog
   alias Pento.Survey
 
   def mount(_params, _session, socket) do
@@ -11,6 +12,7 @@ defmodule PentoWeb.SurveyLive do
       :ok,
       socket
       |> assign_demographic()
+      |> assign_products()
     }
   end
 
@@ -23,7 +25,29 @@ defmodule PentoWeb.SurveyLive do
     }
   end
 
+  def handle_info({:created_rating, updated_product, product_index}, socket) do
+    {:noreply, handle_rating_created(socket, updated_product, product_index)}
+  end
+
+  def handle_rating_created(
+        %{assigns: %{products: products}} = socket,
+        updated_product,
+        product_index
+      ) do
+    socket
+    |> put_flash(:info, "Rating submitted successfully.")
+    |> assign(:products, List.replace_at(products, product_index, updated_product))
+  end
+
   defp assign_demographic(%{assigns: %{current_user: current_user}} = socket) do
     assign(socket, :demographic, Survey.get_demographic_by_user(current_user))
+  end
+
+  defp assign_products(%{assigns: %{current_user: current_user}} = socket) do
+    assign(socket, :products, list_products(current_user))
+  end
+
+  defp list_products(user) do
+    Catalog.list_products_with_user_rating(user)
   end
 end
